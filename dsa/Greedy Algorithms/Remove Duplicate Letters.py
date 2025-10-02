0cbcb20c2c4ef78086dffe56853201c43f0a9e51
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-02 23:55:18 +0530

    Auto-update at 2025-10-02 23:55:18

--- dsa/Greedy Algorithms/Remove Duplicate Letters.py
+++ dsa/Greedy Algorithms/Remove Duplicate Letters.py
@@ -1,16 +1,11 @@
+from collections import Counter
 s = "cbacdcbc"
-r = ""
-d1 = {}
-prev = ""
-next = ""
-for ch in s:
-    
-    if ch in d1 and (ch > prev or ch < prev):
-        del d1[ch]
-    d1[ch] = True
-    prev = ch
-print(d1)
+Counter = Counter(s)
+seen = set()
+stack = []
 
-r = list(d1.keys())
-result = ''.join(map(str,r))
-print(result)
+for ch in s:
+    Counter[ch] -=1
+    if ch in seen:
+        continue
+    
