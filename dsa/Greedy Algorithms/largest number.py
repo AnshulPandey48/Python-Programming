e5c475630adbfd13e247916308ec127047cb8363
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-02 20:08:41 +0530

    Auto-update at 2025-10-02 20:08:41

--- dsa/Greedy Algorithms/largest number.py
+++ dsa/Greedy Algorithms/largest number.py
@@ -1,1 +1,16 @@
-from functools import cmp_to_key 
+from functools import cmp_to_key
+def compare(x,y):
+    if str(x) + str(y) > str(y) + str(x):
+        return -1
+    else:
+        return 1
+    
+def largest_number(nums):
+    nums_sorted = sorted(nums,key=cmp_to_key(compare))
+    largest_number = ''.join(map(str,nums_sorted))
+    if largest_number[0] == "0":
+        return "0"
+    else:
+        return largest_number
+
+print(largest_number([3, 30, 34, 5, 9]))  # "9534330"
