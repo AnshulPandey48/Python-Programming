f05a98b34522ef6a3f74d8940a105033f37174d7
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-03 00:15:25 +0530

    Auto-update at 2025-10-03 00:15:25

--- dsa/Greedy Algorithms/Remove Duplicate Letters.py
+++ dsa/Greedy Algorithms/Remove Duplicate Letters.py
@@ -8,4 +8,12 @@ for ch in s:
     Counter[ch] -=1
     if ch in seen:
         continue
+    while stack and ch < stack[-1] and Counter[stack[-1]] > 0:
+        removed = stack.pop()
+        seen.remove(removed)
     
+    stack.append(ch)
+    seen.add(ch)
+print(stack)
+result = "".join(map(str,stack))
+print(result)
