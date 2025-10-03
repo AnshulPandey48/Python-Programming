ce1dd032fb666274dfe1a03b49d3b89628596ab7
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-03 23:19:09 +0530

    Auto-update at 2025-10-03 23:19:09

--- encoder-decoder/models.py
+++ encoder-decoder/models.py
@@ -5,4 +5,7 @@ class Encoder(nn.Module):
     def __init__(self,input_dim,emb_dim,hidden_dim,n_layers,dropout):
         super.__init__()
         self.embedding = nn.Embedding(input_dim,emb_dim) #return a tensor containing the embeddings
-        self.lstm = nn.LSTM(emb_dim,)
+        self.lstm = nn.LSTM(emb_dim,hidden_dim,n_layers,dropout=dropout,batch_first=True)
+    def forward(self,src):
+        embedded = self.embedding(src)
+        output,(hidden,cell) = 
