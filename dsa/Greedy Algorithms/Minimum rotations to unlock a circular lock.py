53bcc8e1744e6d6bb09fa48ed586b47ad428a075
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-06 07:18:31 +0530

    Auto-update at 2025-10-06 07:18:31

--- dsa/Greedy Algorithms/Minimum rotations to unlock a circular lock.py
+++ dsa/Greedy Algorithms/Minimum rotations to unlock a circular lock.py
@@ -1,2 +1,10 @@
 R = 222
 D = 333
+count = 0
+digit1 = [int(digit) for digit in str(R)]
+digit2 = [int(digit) for digit  in str(D)]
+
+# 2 , 9    9 , 2
+diff1 = abs(digit1-digit2)
+diff2 = min(abs((9-digit1)))
+count += min(diff,)
