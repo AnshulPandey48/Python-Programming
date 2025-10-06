fe1d28a65916059c55eb864bec1d9a2f7e725e1d
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-06 08:44:09 +0530

    Auto-update at 2025-10-06 08:44:09

--- dsa/Greedy Algorithms/Smallest Subset with Greater Sum.py
+++ dsa/Greedy Algorithms/Smallest Subset with Greater Sum.py
@@ -1,6 +1,6 @@
 arr = [66,53,87,23 ,58 ,19]
 arr.sort(reverse=True)
-print()
+print(arr)
 count = 1
 i = 0
 j = 1
@@ -8,7 +8,7 @@ n = len(arr)
 origi = arr[i]
 while n > 0:
     maxi = sum((arr[j:n])) # j is starting n is len(arr) -1
-    if origi < maxi:
+    if origi <= maxi:
         j+=1
         count+=1
         i+=1
