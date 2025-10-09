1322641c85c9a509b218ae9ac675a4a301f2e328
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-10 00:21:02 +0530

    Auto-update at 2025-10-10 00:21:02

--- dsa/Greedy Algorithms/Jump Game.py
+++ dsa/Greedy Algorithms/Jump Game.py
@@ -1,9 +1,11 @@
-arr = [1,2,0,3,0,0]
+arr = [1,0,2]
 curr_pos = 0
 poss_steps = arr[0]
 
-while curr_pos <= len(arr):
+while curr_pos <= len(arr)-1:
     if arr[curr_pos] == 0:
         print(False)
         exit()
-        
+    curr_pos += arr[curr_pos]
+
+print(True)
