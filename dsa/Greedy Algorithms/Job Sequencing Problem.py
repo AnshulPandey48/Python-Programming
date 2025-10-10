e4a4adf5aab70698fba4681f831b5b34785ed846
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-10 20:27:57 +0530

    Auto-update at 2025-10-10 20:27:57

--- dsa/Greedy Algorithms/Job Sequencing Problem.py
+++ dsa/Greedy Algorithms/Job Sequencing Problem.py
@@ -1,17 +1,17 @@
-deadline = [2,1,2,1,1]
-profit = [100,19,27,25,15]
-res = []
+deadline = [3,1,2,2]
+profit = [50,15,20,30]
+deadline_profit = [(i,d,p) for i , (d,p) in enumerate(zip(deadline,profit))]
+deadline_profit.sort(reverse= True,key = lambda x : x[2])
 count = 0
-total_profit = 0
-deadline_profit = [(i,d,p) for i ,(d,p) in enumerate(zip(deadline,profit))]
-deadline_profit.sort(key= lambda x: x[1])
-print(deadline_profit)
-sorted_array = sorted(deadline_profit[:-1],reverse=True)
-deadline_profit = sorted_array + [deadline_profit[-1]]
-for i in range(len(deadline_profit)):
-    if deadline_profit[i][1] >  count:  
-        total_profit += deadline_profit[i][2]
-        count+=1
+res = []
+i = 0
+sum1 = 0
+while i < len(deadline_profit):
+    if count < deadline_profit[i][1]:
+        count +=1
+        sum1 += deadline_profit[i][2]
+    i+=1
 res.append(count)
-res.append(total_profit)
+res.append(sum1)
 print(res)
+
