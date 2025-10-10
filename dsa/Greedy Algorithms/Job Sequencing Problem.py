92520e023d34e79ebc0c3eee36b10e9db8790685
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-10 23:09:20 +0530

    Auto-update at 2025-10-10 23:09:20

--- dsa/Greedy Algorithms/Job Sequencing Problem.py
+++ dsa/Greedy Algorithms/Job Sequencing Problem.py
@@ -3,8 +3,23 @@ profit = [50,15,20,30]
 deadline_profit = [(i,d,p) for i , (d,p) in enumerate(zip(deadline,profit))]
 deadline_profit.sort(reverse= True,key = lambda x : x[2])
 count = 0
+sum1 = 0
 max_index = max(deadline)
 slots = [None]*(max_index+1) # index 0 ... max_index
 index_place = 0
-while count != len(deadline_profit):
-    if 
+i = 0
+while i != len(deadline_profit):
+    index_place = deadline_profit[i][1]
+    if slots[index_place] is None:
+        slots[index_place] = deadline_profit[i][2]
+        count +=1
+        sum1 += deadline_profit[i][2]
+    else:
+        index_place -=1
+        slots[index_place] = deadline_profit[i][2]
+        sum1 += deadline_profit[i][2]
+        count +=1
+    i+=1
+print(count)
+print(sum1)
+
