ace3f8b44525190762a51d5a70e5c9aa1fa7f83d
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-10 23:04:18 +0530

    Auto-update at 2025-10-10 23:04:18

--- dsa/Greedy Algorithms/Job Sequencing Problem.py
+++ dsa/Greedy Algorithms/Job Sequencing Problem.py
@@ -1,2 +1,10 @@
-arr = [4, 1, 1, 1]
-profit = [20, 10, 40, 30]
+deadline = [3,1,2,2]
+profit = [50,15,20,30]
+deadline_profit = [(i,d,p) for i , (d,p) in enumerate(zip(deadline,profit))]
+deadline_profit.sort(reverse= True,key = lambda x : x[2])
+count = 0
+max_index = max(deadline)
+slots = [None]*(max_index+1) # index 0 ... max_index
+index_place = 0
+while count != len(deadline_profit):
+    if 
