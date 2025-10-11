f67c817800ec4104a9f4a18ac0a66af2c6eb1c09
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-11 22:38:24 +0530

    Auto-update at 2025-10-11 22:38:24

--- dsa/Greedy Algorithms/Minimum Fibonacci terms with sum equal to K.py
+++ dsa/Greedy Algorithms/Minimum Fibonacci terms with sum equal to K.py
@@ -1,2 +1,11 @@
 k = 7
 # generating fibo nachi series up to 7
+l = 0
+r = 7
+start = 0
+nextt = 1
+for i in range(7):
+    start = start + nextt
+    nextt = nextt + start
+    print(start)
+    print(nextt)
