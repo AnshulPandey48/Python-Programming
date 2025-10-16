b429349d2f8eaf505c62d78f6c7ebe3361c03fc8
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-16 21:41:01 +0530

    Auto-update at 2025-10-16 21:41:01

--- dsa/Greedy Algorithms/smallest number.py
+++ dsa/Greedy Algorithms/smallest number.py
@@ -1,21 +1,6 @@
 s = 9
 d = 2
-value = 1
-sd = [value]*d
-print(sd)
-sum1 = 0
-i = d
-while i > 0:
-    sum1 = sum(sd)
-    if sum1 < s:
-        newv = s - sum1
-        if newv > 9:
-            sd[i] = 9
-            sd[i-1] = sum1 - 9
-        else:
-            sd[i] = s - sum1
-    if sum1 == s:
-        print(sd)
-        exit()
-print(-1)
+if (s == 0 and d > 1) or s > 9 * d:
+    print(-1)
+    exit()
     
