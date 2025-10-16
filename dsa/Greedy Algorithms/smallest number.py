c8ced1b0c7350eb5f6cd41bd5c13a39c9b54fed3
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-16 22:21:21 +0530

    Auto-update at 2025-10-16 22:21:21

--- dsa/Greedy Algorithms/smallest number.py
+++ dsa/Greedy Algorithms/smallest number.py
@@ -1,19 +1,7 @@
 s = 9
 d = 2
-if (s == 0 and d > 1) or s > 9 * d: # edge case 
+#handling edge cases 
+if (s == 0 and d < 1) or (s > 9*d):
     print(-1)
     exit()
-else:
-    sd = [0]*d
-    s-=1
-    sd[0] = 1
-    print(sd)
-
-    i = d -1
-    while i >= 0 and s> 0:
-        add = min(9 - sd[i],s)
-        sd[i] += add
-        s-= add
-        i -=1 
-print(sd)
-print("Smallest number: ",''.join (map(str,sd)))
+    
