a7fc08671bbef5ae15e098f9bebea61cb7ff199c
Anshul Pandey <131686171+AnshulPandey48@users.noreply.github.com>
2025-10-20 00:25:55 +0530

    Auto-update at 2025-10-20 00:25:55

--- dsa/Slidding Window/Smallest window containing 0, 1 and 2.py
+++ dsa/Slidding Window/Smallest window containing 0, 1 and 2.py
@@ -1,16 +1,4 @@
-def smallestsubstring(s):
-    left = 0
-    count = {'0':0,'1':0,'2':0}
-    min_len = float('inf')
-    for right in range(len(s)):
-        count(s[right])+=1
-    while count['0'] > 0 and count['1'] > 0 and count['2'] > 0:
-        min_len = min(min_len,right - left+1)
-        count(s[left])-=1
-        left +=1
-
-    if min_len != float('inf'):
-        print(min_len)
-    else:
-        print(-1)
-
+def smallest_win(s):
+    #some sort ofdictionary created
+    count = {'0' : 0 , '1' : 0,'2':0}
+    for right in s:
